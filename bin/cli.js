7478090b90747f0799ad9c91321700a26f4cccf9
王强 <wangqiang@feewee.cn>
2024-02-19 15:28:19 +0800

    fix

--- bin/cli.js
+++ bin/cli.js
@@ -3,13 +3,17 @@ const { program } = require("commander");
 const chalk = require("chalk");
 const figlet = require("figlet");
 
+program.on("--help", function (...args) {
+  console.log(figlet.textSync("Q CLI", { font: "3D-ASCII" }));
+});
+
 program
   .command("create <app-name>")
   .description("create a new project")
   .option("-f --force", "overwrite target directory if it already exists")
   .action((name, options) => {
     console.log();
-    figlet("WELCOME TO USE Q CLI", function (err, data) {
+    figlet("WELCOME TO USE Q CLI", { font: "3D-ASCII" }, function (err, data) {
       if (err) {
         console.log("Something went wrong...");
         console.dir(err);
